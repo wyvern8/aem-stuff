89594a4ec766fe88d2989cc6637275a1d1414686
wyvern8 <wyvern8@users.noreply.github.com>
2017-11-08 17:33:15 +1100

    Update extractAemAuthorizables.sh

--- extractAemAuthorizables.sh
+++ extractAemAuthorizables.sh
@@ -7,9 +7,14 @@ display_usage() {
     echo "eg. ./extractAemAuthorizables.sh http://localhost:4502 admin admin"
 }
 
+if ! type "jq" > /dev/null; then
+    echo "This script requires the 'jq' command installed on path from https://stedolan.github.io/jq"
+    exit 1
+fi
+
 if [ $# -lt 3 ]; then
     display_usage
     exit 1
 fi
 
-curl -s -u $2:$3 "$1/bin/querybuilder.json?path=/home&p.limit=-1&p.hits=full&property=jcr:primaryType&property.1_value=rep:Group&property.2_value=rep:User" 2>&1 | sed 's/jcr\://g' | sed 's/rep\://g' |jq '.hits | map([.primaryType, .principalName, .path] | join(", ")) | join("\n")' | sed 's/\\n/\n/g' | sed 's/"//g' | sort
+curl -sk -u $2:$3 "$1/bin/querybuilder.json?path=/home&p.limit=-1&p.hits=full&property=jcr:primaryType&property.1_value=rep:Group&property.2_value=rep:User" 2>&1 | sed 's/jcr\://g' | sed 's/rep\://g' |jq '.hits | map([.primaryType, .principalName, .path] | join(", ")) | join("\n")' | sed 's/\\n/\n/g' | sed 's/"//g' | sort
