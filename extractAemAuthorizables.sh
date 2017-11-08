f8a8f917160e129695b5d6d2d108ff81a9d86f3d
wyvern8 <wyvern8@users.noreply.github.com>
2017-11-08 18:17:34 +1100

    Update extractAemAuthorizables.sh

--- extractAemAuthorizables.sh
+++ extractAemAuthorizables.sh
@@ -19,7 +19,8 @@ fi
 
 echo "type, principalName, path"
 # curl aem querybuilder requesting nodes under /home of jcr:primaryType = rep:Group or rep:User ..
-curl -sk -u $2:$3 "$1/bin/querybuilder.json?path=/home&p.limit=-1&p.hits=full&property=jcr:primaryType&property.1_value=rep:Group&property.2_value=rep:User" \
+curl -skG -u $2:$3 "$1/bin/querybuilder.json" \
+-d "path=/home&p.limit=-1&p.hits=full&property=jcr:primaryType&property.1_value=rep:Group&property.2_value=rep:User" \
 2>&1 | sed 's/jcr\://g' | sed 's/rep\://g' | # ..strip attribute namespaces causing jq to choke when mapping..
 jq '.hits | map([.primaryType, .principalName, .path] | join(", ")) | join("\n")' | # .. and use jq to map results to csv..
 sed 's/\\n/\n/g' | sed 's/"//g' | sort # .. then clean up csv linefeeds and quotes, and sort. done!
